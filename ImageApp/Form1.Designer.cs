d4c681a7e79d10af528737044d7f7f4d1f7934d2
mabaranowski <mateusz.m.baranowski@gmail.com>
2019-03-16 10:48:45 +0100

    Lab01

---
+++ ImageApp/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace ImageApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openButton = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.saveAsButton = new System.Windows.Forms.Button();
+            this.openAsButton = new System.Windows.Forms.Button();
+            this.widthTextBox = new System.Windows.Forms.TextBox();
+            this.heightTextBox = new System.Windows.Forms.TextBox();
+            this.vResTextBox = new System.Windows.Forms.TextBox();
+            this.hResTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(37, 31);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(111, 42);
+            this.openButton.TabIndex = 0;
+            this.openButton.Text = "Open";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(37, 253);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(410, 440);
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(154, 31);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(111, 42);
+            this.buttonSave.TabIndex = 2;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(271, 31);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(111, 42);
+            this.clearButton.TabIndex = 3;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // saveAsButton
+            // 
+            this.saveAsButton.Location = new System.Drawing.Point(154, 79);
+            this.saveAsButton.Name = "saveAsButton";
+            this.saveAsButton.Size = new System.Drawing.Size(111, 42);
+            this.saveAsButton.TabIndex = 4;
+            this.saveAsButton.Text = "Save As";
+            this.saveAsButton.UseVisualStyleBackColor = true;
+            this.saveAsButton.Click += new System.EventHandler(this.saveAsButton_Click);
+            // 
+            // openAsButton
+            // 
+            this.openAsButton.Location = new System.Drawing.Point(37, 79);
+            this.openAsButton.Name = "openAsButton";
+            this.openAsButton.Size = new System.Drawing.Size(111, 42);
+            this.openAsButton.TabIndex = 5;
+            this.openAsButton.Text = "Open As";
+            this.openAsButton.UseVisualStyleBackColor = true;
+            this.openAsButton.Click += new System.EventHandler(this.openAsButton_Click);
+            // 
+            // widthTextBox
+            // 
+            this.widthTextBox.Enabled = false;
+            this.widthTextBox.Location = new System.Drawing.Point(483, 53);
+            this.widthTextBox.Name = "widthTextBox";
+            this.widthTextBox.Size = new System.Drawing.Size(100, 20);
+            this.widthTextBox.TabIndex = 6;
+            this.widthTextBox.TextChanged += new System.EventHandler(this.widthTextBox_TextChanged);
+            // 
+            // heightTextBox
+            // 
+            this.heightTextBox.Enabled = false;
+            this.heightTextBox.Location = new System.Drawing.Point(483, 101);
+            this.heightTextBox.Name = "heightTextBox";
+            this.heightTextBox.Size = new System.Drawing.Size(100, 20);
+            this.heightTextBox.TabIndex = 7;
+            // 
+            // vResTextBox
+            // 
+            this.vResTextBox.Enabled = false;
+            this.vResTextBox.Location = new System.Drawing.Point(614, 101);
+            this.vResTextBox.Name = "vResTextBox";
+            this.vResTextBox.Size = new System.Drawing.Size(100, 20);
+            this.vResTextBox.TabIndex = 8;
+            // 
+            // hResTextBox
+            // 
+            this.hResTextBox.Enabled = false;
+            this.hResTextBox.Location = new System.Drawing.Point(614, 53);
+            this.hResTextBox.Name = "hResTextBox";
+            this.hResTextBox.Size = new System.Drawing.Size(100, 20);
+            this.hResTextBox.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(515, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Width";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(515, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Height";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(625, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "  Vertical Res";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(625, 85);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Horizontal Res";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1136, 724);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.hResTextBox);
+            this.Controls.Add(this.vResTextBox);
+            this.Controls.Add(this.heightTextBox);
+            this.Controls.Add(this.widthTextBox);
+            this.Controls.Add(this.openAsButton);
+            this.Controls.Add(this.saveAsButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.openButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button saveAsButton;
+        private System.Windows.Forms.Button openAsButton;
+        private System.Windows.Forms.TextBox widthTextBox;
+        private System.Windows.Forms.TextBox heightTextBox;
+        private System.Windows.Forms.TextBox vResTextBox;
+        private System.Windows.Forms.TextBox hResTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ ImageApp/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Created by Mateusz Baranowski [AIR]
+
+namespace ImageApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            Image myImage = Image.FromFile(@"c:\UNI\VS_Projects\Resources\Juno.jpg");
+            pictureBox.Image = myImage;
+            this.pictureBox.SizeMode = PictureBoxSizeMode.AutoSize;
+
+            clearFields();
+            widthTextBox.Text = Convert.ToString(pictureBox.Width);
+            heightTextBox.Text = Convert.ToString(pictureBox.Height);
+            vResTextBox.Text = Convert.ToString(pictureBox.Image.VerticalResolution);
+            hResTextBox.Text = Convert.ToString(pictureBox.Image.HorizontalResolution);
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            if (pictureBox.Image != null)
+            {
+                pictureBox.Image.Save(@"c:\UNI\VS_Projects\Resources\SaveFolder\Juno01.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+                pictureBox.Image.Save(@"c:\UNI\VS_Projects\Resources\SaveFolder\Juno01.png", System.Drawing.Imaging.ImageFormat.Png);
+                pictureBox.Image.Save(@"c:\UNI\VS_Projects\Resources\SaveFolder\Juno01.bmp", System.Drawing.Imaging.ImageFormat.Bmp);
+                pictureBox.Image.Save(@"c:\UNI\VS_Projects\Resources\SaveFolder\Juno01.gif", System.Drawing.Imaging.ImageFormat.Gif);
+                pictureBox.Image.Save(@"c:\UNI\VS_Projects\Resources\SaveFolder\Juno01.tif", System.Drawing.Imaging.ImageFormat.Tiff);
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            pictureBox.Image = null;
+            pictureBox.Invalidate();
+            clearFields();
+        }
+
+        private void saveAsButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Jpeg Image|*.jpg|Png Image|*.png|Bitmap Image|*.bmp|Gif Image|*.gif|Tiff Image|*.tiff";
+            saveFileDialog.Title = "Save as an Image File";
+            saveFileDialog.ShowDialog();
+
+            if (saveFileDialog.FileName != "" && pictureBox.Image != null)
+            {
+                System.IO.FileStream fileStream = (System.IO.FileStream)saveFileDialog.OpenFile();
+
+                switch (saveFileDialog.FilterIndex)
+                {
+                    case 1:
+                        pictureBox.Image.Save(fileStream, System.Drawing.Imaging.ImageFormat.Jpeg);
+                        break;
+                    
+                    case 2:
+                        pictureBox.Image.Save(fileStream, System.Drawing.Imaging.ImageFormat.Png);
+                        break;
+                    
+                    case 3:
+                        pictureBox.Image.Save(fileStream, System.Drawing.Imaging.ImageFormat.Bmp);
+                        break;
+
+                    case 4:
+                        pictureBox.Image.Save(fileStream, System.Drawing.Imaging.ImageFormat.Gif);
+                        break;
+
+                    case 5:
+                        pictureBox.Image.Save(fileStream, System.Drawing.Imaging.ImageFormat.Tiff);
+                        break;
+                }
+            }
+        }
+
+        private void openAsButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Jpeg Image|*.jpg|Png Image|*.png|Bitmap Image|*.bmp|Gif Image|*.gif|Tiff Image|*.tiff";
+            openFileDialog.Title = "Open from an Image File";
+            openFileDialog.ShowDialog();
+
+            System.IO.FileStream fileStream = (System.IO.FileStream)openFileDialog.OpenFile();
+
+            Image myImage = Image.FromFile(openFileDialog.InitialDirectory + openFileDialog.FileName);
+            pictureBox.Image = myImage;
+            this.pictureBox.SizeMode = PictureBoxSizeMode.AutoSize;
+
+            clearFields();
+            widthTextBox.Text = Convert.ToString(pictureBox.Width);
+            heightTextBox.Text = Convert.ToString(pictureBox.Height);
+            vResTextBox.Text = Convert.ToString(pictureBox.Image.VerticalResolution);
+            hResTextBox.Text = Convert.ToString(pictureBox.Image.HorizontalResolution);
+        }
+
+        private void widthTextBox_TextChanged(object sender, EventArgs e)
+        {
+            //to delete
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            //to delete
+        }
+
+        private void clearFields()
+        {
+            widthTextBox.Text = Convert.ToString("");
+            heightTextBox.Text = Convert.ToString("");
+            vResTextBox.Text = Convert.ToString("");
+            hResTextBox.Text = Convert.ToString("");
+        }
+    }
+}
