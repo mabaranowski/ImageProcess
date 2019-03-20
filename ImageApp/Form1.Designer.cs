6ae7c3dc0d8e35b5968425e069a99b6a057399f1
mabaranowski <mateusz.m.baranowski@gmail.com>
2019-03-20 22:13:52 +0100

    Lab02

--- ImageApp/Form1.Designer.cs
+++ ImageApp/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ImageApp
 {
-    partial class Form1
+    partial class ImageProcessApp
     {
         /// <summary>
         /// Required designer variable.
@@ -42,12 +42,29 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.colorsTextBox = new System.Windows.Forms.TextBox();
+            this.detailsListBox = new System.Windows.Forms.ListBox();
+            this.cropButton = new System.Windows.Forms.Button();
+            this.cropWidthTextBox = new System.Windows.Forms.TextBox();
+            this.cropHeightTextBox = new System.Windows.Forms.TextBox();
+            this.cropPosX = new System.Windows.Forms.TextBox();
+            this.cropPosY = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.openUrlButton = new System.Windows.Forms.Button();
+            this.urlTextBox = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.lineShape1 = new Microsoft.VisualBasic.PowerPacks.LineShape();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // openButton
             // 
-            this.openButton.Location = new System.Drawing.Point(37, 31);
+            this.openButton.Location = new System.Drawing.Point(22, 21);
             this.openButton.Name = "openButton";
             this.openButton.Size = new System.Drawing.Size(111, 42);
             this.openButton.TabIndex = 0;
@@ -57,15 +74,15 @@
             // 
             // pictureBox
             // 
-            this.pictureBox.Location = new System.Drawing.Point(37, 253);
+            this.pictureBox.Location = new System.Drawing.Point(22, 224);
             this.pictureBox.Name = "pictureBox";
-            this.pictureBox.Size = new System.Drawing.Size(410, 440);
+            this.pictureBox.Size = new System.Drawing.Size(911, 802);
             this.pictureBox.TabIndex = 1;
             this.pictureBox.TabStop = false;
             // 
             // buttonSave
             // 
-            this.buttonSave.Location = new System.Drawing.Point(154, 31);
+            this.buttonSave.Location = new System.Drawing.Point(139, 22);
             this.buttonSave.Name = "buttonSave";
             this.buttonSave.Size = new System.Drawing.Size(111, 42);
             this.buttonSave.TabIndex = 2;
@@ -75,7 +92,7 @@
             // 
             // clearButton
             // 
-            this.clearButton.Location = new System.Drawing.Point(271, 31);
+            this.clearButton.Location = new System.Drawing.Point(256, 22);
             this.clearButton.Name = "clearButton";
             this.clearButton.Size = new System.Drawing.Size(111, 42);
             this.clearButton.TabIndex = 3;
@@ -85,7 +102,7 @@
             // 
             // saveAsButton
             // 
-            this.saveAsButton.Location = new System.Drawing.Point(154, 79);
+            this.saveAsButton.Location = new System.Drawing.Point(139, 69);
             this.saveAsButton.Name = "saveAsButton";
             this.saveAsButton.Size = new System.Drawing.Size(111, 42);
             this.saveAsButton.TabIndex = 4;
@@ -95,7 +112,7 @@
             // 
             // openAsButton
             // 
-            this.openAsButton.Location = new System.Drawing.Point(37, 79);
+            this.openAsButton.Location = new System.Drawing.Point(22, 69);
             this.openAsButton.Name = "openAsButton";
             this.openAsButton.Size = new System.Drawing.Size(111, 42);
             this.openAsButton.TabIndex = 5;
@@ -106,16 +123,15 @@
             // widthTextBox
             // 
             this.widthTextBox.Enabled = false;
-            this.widthTextBox.Location = new System.Drawing.Point(483, 53);
+            this.widthTextBox.Location = new System.Drawing.Point(715, 33);
             this.widthTextBox.Name = "widthTextBox";
             this.widthTextBox.Size = new System.Drawing.Size(100, 20);
             this.widthTextBox.TabIndex = 6;
-            this.widthTextBox.TextChanged += new System.EventHandler(this.widthTextBox_TextChanged);
             // 
             // heightTextBox
             // 
             this.heightTextBox.Enabled = false;
-            this.heightTextBox.Location = new System.Drawing.Point(483, 101);
+            this.heightTextBox.Location = new System.Drawing.Point(715, 81);
             this.heightTextBox.Name = "heightTextBox";
             this.heightTextBox.Size = new System.Drawing.Size(100, 20);
             this.heightTextBox.TabIndex = 7;
@@ -123,7 +139,7 @@
             // vResTextBox
             // 
             this.vResTextBox.Enabled = false;
-            this.vResTextBox.Location = new System.Drawing.Point(614, 101);
+            this.vResTextBox.Location = new System.Drawing.Point(832, 81);
             this.vResTextBox.Name = "vResTextBox";
             this.vResTextBox.Size = new System.Drawing.Size(100, 20);
             this.vResTextBox.TabIndex = 8;
@@ -131,7 +147,7 @@
             // hResTextBox
             // 
             this.hResTextBox.Enabled = false;
-            this.hResTextBox.Location = new System.Drawing.Point(614, 53);
+            this.hResTextBox.Location = new System.Drawing.Point(832, 33);
             this.hResTextBox.Name = "hResTextBox";
             this.hResTextBox.Size = new System.Drawing.Size(100, 20);
             this.hResTextBox.TabIndex = 9;
@@ -139,7 +155,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(515, 37);
+            this.label1.Location = new System.Drawing.Point(749, 17);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 10;
@@ -148,7 +164,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(515, 85);
+            this.label2.Location = new System.Drawing.Point(746, 65);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(38, 13);
             this.label2.TabIndex = 11;
@@ -157,27 +173,187 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(625, 37);
+            this.label3.Location = new System.Drawing.Point(843, 17);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.Size = new System.Drawing.Size(67, 13);
             this.label3.TabIndex = 12;
-            this.label3.Text = "  Vertical Res";
-            this.label3.Click += new System.EventHandler(this.label3_Click);
+            this.label3.Text = " Vertical Res";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(625, 85);
+            this.label4.Location = new System.Drawing.Point(843, 65);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(76, 13);
             this.label4.TabIndex = 13;
             this.label4.Text = "Horizontal Res";
             // 
-            // Form1
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(805, 110);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Colors";
+            // 
+            // colorsTextBox
+            // 
+            this.colorsTextBox.Enabled = false;
+            this.colorsTextBox.Location = new System.Drawing.Point(773, 126);
+            this.colorsTextBox.Name = "colorsTextBox";
+            this.colorsTextBox.Size = new System.Drawing.Size(100, 20);
+            this.colorsTextBox.TabIndex = 15;
+            // 
+            // detailsListBox
+            // 
+            this.detailsListBox.FormattingEnabled = true;
+            this.detailsListBox.Location = new System.Drawing.Point(373, 21);
+            this.detailsListBox.Name = "detailsListBox";
+            this.detailsListBox.Size = new System.Drawing.Size(323, 134);
+            this.detailsListBox.TabIndex = 16;
+            // 
+            // cropButton
+            // 
+            this.cropButton.Location = new System.Drawing.Point(373, 161);
+            this.cropButton.Name = "cropButton";
+            this.cropButton.Size = new System.Drawing.Size(100, 46);
+            this.cropButton.TabIndex = 17;
+            this.cropButton.Text = "Crop";
+            this.cropButton.UseVisualStyleBackColor = true;
+            this.cropButton.Click += new System.EventHandler(this.cropButton_Click);
+            // 
+            // cropWidthTextBox
+            // 
+            this.cropWidthTextBox.Location = new System.Drawing.Point(479, 187);
+            this.cropWidthTextBox.Name = "cropWidthTextBox";
+            this.cropWidthTextBox.Size = new System.Drawing.Size(100, 20);
+            this.cropWidthTextBox.TabIndex = 18;
+            // 
+            // cropHeightTextBox
+            // 
+            this.cropHeightTextBox.Location = new System.Drawing.Point(596, 187);
+            this.cropHeightTextBox.Name = "cropHeightTextBox";
+            this.cropHeightTextBox.Size = new System.Drawing.Size(100, 20);
+            this.cropHeightTextBox.TabIndex = 19;
+            // 
+            // cropPosX
+            // 
+            this.cropPosX.Location = new System.Drawing.Point(715, 187);
+            this.cropPosX.Name = "cropPosX";
+            this.cropPosX.Size = new System.Drawing.Size(100, 20);
+            this.cropPosX.TabIndex = 20;
+            // 
+            // cropPosY
+            // 
+            this.cropPosY.Location = new System.Drawing.Point(833, 187);
+            this.cropPosY.Name = "cropPosY";
+            this.cropPosY.Size = new System.Drawing.Size(100, 20);
+            this.cropPosY.TabIndex = 21;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(625, 171);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 13);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Height";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(511, 171);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 23;
+            this.label7.Text = "Width";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(748, 171);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(32, 13);
+            this.label8.TabIndex = 24;
+            this.label8.Text = "PosX";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(865, 171);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(32, 13);
+            this.label9.TabIndex = 25;
+            this.label9.Text = "PosY";
+            // 
+            // openUrlButton
+            // 
+            this.openUrlButton.Location = new System.Drawing.Point(256, 69);
+            this.openUrlButton.Name = "openUrlButton";
+            this.openUrlButton.Size = new System.Drawing.Size(111, 42);
+            this.openUrlButton.TabIndex = 26;
+            this.openUrlButton.Text = "Open URL";
+            this.openUrlButton.UseVisualStyleBackColor = true;
+            this.openUrlButton.Click += new System.EventHandler(this.openUrlButton_Click);
+            // 
+            // urlTextBox
+            // 
+            this.urlTextBox.Location = new System.Drawing.Point(22, 135);
+            this.urlTextBox.Name = "urlTextBox";
+            this.urlTextBox.Size = new System.Drawing.Size(345, 20);
+            this.urlTextBox.TabIndex = 27;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(19, 119);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(29, 13);
+            this.label11.TabIndex = 29;
+            this.label11.Text = "URL";
+            // 
+            // shapeContainer1
+            // 
+            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.lineShape1});
+            this.shapeContainer1.Size = new System.Drawing.Size(1904, 1041);
+            this.shapeContainer1.TabIndex = 30;
+            this.shapeContainer1.TabStop = false;
+            // 
+            // lineShape1
+            // 
+            this.lineShape1.BorderWidth = 2;
+            this.lineShape1.Name = "lineShape1";
+            this.lineShape1.X1 = 951;
+            this.lineShape1.X2 = 950;
+            this.lineShape1.Y1 = 22;
+            this.lineShape1.Y2 = 1025;
+            // 
+            // ImageProcessApp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1136, 724);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.urlTextBox);
+            this.Controls.Add(this.openUrlButton);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cropPosY);
+            this.Controls.Add(this.cropPosX);
+            this.Controls.Add(this.cropHeightTextBox);
+            this.Controls.Add(this.cropWidthTextBox);
+            this.Controls.Add(this.cropButton);
+            this.Controls.Add(this.detailsListBox);
+            this.Controls.Add(this.colorsTextBox);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -192,8 +368,10 @@
             this.Controls.Add(this.buttonSave);
             this.Controls.Add(this.pictureBox);
             this.Controls.Add(this.openButton);
-            this.Name = "Form1";
+            this.Controls.Add(this.shapeContainer1);
+            this.Name = "ImageProcessApp";
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -216,6 +394,23 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox colorsTextBox;
+        private System.Windows.Forms.ListBox detailsListBox;
+        private System.Windows.Forms.Button cropButton;
+        private System.Windows.Forms.TextBox cropWidthTextBox;
+        private System.Windows.Forms.TextBox cropHeightTextBox;
+        private System.Windows.Forms.TextBox cropPosX;
+        private System.Windows.Forms.TextBox cropPosY;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button openUrlButton;
+        private System.Windows.Forms.TextBox urlTextBox;
+        private System.Windows.Forms.Label label11;
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private Microsoft.VisualBasic.PowerPacks.LineShape lineShape1;
     }
 }
 
--- ImageApp/Form1.cs
+++ ImageApp/Form1.cs
@@ -3,33 +3,39 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
 
 //Created by Mateusz Baranowski [AIR]
 
 namespace ImageApp
 {
-    public partial class Form1 : Form
+    public partial class ImageProcessApp : Form
     {
-        public Form1()
+
+        private Image image;
+        private String path;
+
+        public ImageProcessApp()
         {
             InitializeComponent();
         }
 
         private void openButton_Click(object sender, EventArgs e)
         {
-            Image myImage = Image.FromFile(@"c:\UNI\VS_Projects\Resources\Juno.jpg");
-            pictureBox.Image = myImage;
-            this.pictureBox.SizeMode = PictureBoxSizeMode.AutoSize;
+            path = @"c:\UNI\VS_Projects\Resources\Juno.jpg";
+            image = Image.FromFile(path);
+           
+            pictureBox.Image = image;
+            pictureBox.SizeMode = PictureBoxSizeMode.Normal;
 
             clearFields();
-            widthTextBox.Text = Convert.ToString(pictureBox.Width);
-            heightTextBox.Text = Convert.ToString(pictureBox.Height);
-            vResTextBox.Text = Convert.ToString(pictureBox.Image.VerticalResolution);
-            hResTextBox.Text = Convert.ToString(pictureBox.Image.HorizontalResolution);
+            populateFields(path);
+            details(path);
         }
 
         private void saveButton_Click(object sender, EventArgs e)
@@ -66,20 +72,16 @@ namespace ImageApp
                 {
                     case 1:
                         pictureBox.Image.Save(fileStream, System.Drawing.Imaging.ImageFormat.Jpeg);
-                        break;
-                    
+                        break; 
                     case 2:
                         pictureBox.Image.Save(fileStream, System.Drawing.Imaging.ImageFormat.Png);
-                        break;
-                    
+                        break;                   
                     case 3:
                         pictureBox.Image.Save(fileStream, System.Drawing.Imaging.ImageFormat.Bmp);
                         break;
-
                     case 4:
                         pictureBox.Image.Save(fileStream, System.Drawing.Imaging.ImageFormat.Gif);
                         break;
-
                     case 5:
                         pictureBox.Image.Save(fileStream, System.Drawing.Imaging.ImageFormat.Tiff);
                         break;
@@ -95,26 +97,56 @@ namespace ImageApp
             openFileDialog.ShowDialog();
 
             System.IO.FileStream fileStream = (System.IO.FileStream)openFileDialog.OpenFile();
+            path = openFileDialog.InitialDirectory + openFileDialog.FileName;
 
-            Image myImage = Image.FromFile(openFileDialog.InitialDirectory + openFileDialog.FileName);
-            pictureBox.Image = myImage;
-            this.pictureBox.SizeMode = PictureBoxSizeMode.AutoSize;
-
+            pictureBox.SizeMode = PictureBoxSizeMode.Normal;
+            image = Image.FromFile(path);
+            pictureBox.Image = image;
+            
             clearFields();
-            widthTextBox.Text = Convert.ToString(pictureBox.Width);
-            heightTextBox.Text = Convert.ToString(pictureBox.Height);
-            vResTextBox.Text = Convert.ToString(pictureBox.Image.VerticalResolution);
-            hResTextBox.Text = Convert.ToString(pictureBox.Image.HorizontalResolution);
+            populateFields(path);
+            details(path);
         }
 
-        private void widthTextBox_TextChanged(object sender, EventArgs e)
+        private void openUrlButton_Click(object sender, EventArgs e)
         {
-            //to delete
+            if (!string.IsNullOrWhiteSpace(urlTextBox.Text))
+            {
+                pictureBox.SizeMode = PictureBoxSizeMode.Normal;
+                try
+                {
+                    pictureBox.Load(urlTextBox.Text);
+                    image = pictureBox.Image;
+
+                    path = @"c:\UNI\VS_Projects\Resources\UrlTemp\tmp";
+                    pictureBox.Image.Save(path);
+
+                    clearFields();
+                    populateFields(path);
+                    details(path);
+                }
+                catch
+                {
+                    System.Diagnostics.Debug.WriteLine("Error opening image from URL");
+                }
+            }
         }
 
-        private void label3_Click(object sender, EventArgs e)
+        private void cropButton_Click(object sender, EventArgs e)
         {
-            //to delete
+            if (!string.IsNullOrWhiteSpace(cropWidthTextBox.Text) && !string.IsNullOrWhiteSpace(cropHeightTextBox.Text) && !string.IsNullOrWhiteSpace(cropPosX.Text) && !string.IsNullOrWhiteSpace(cropPosY.Text))
+            {
+                try
+                {
+                    cropImage(image, Convert.ToInt16(cropWidthTextBox.Text), Convert.ToInt16(cropHeightTextBox.Text), Convert.ToInt16(cropPosX.Text), Convert.ToInt16(cropPosY.Text));
+                    pictureBox.SizeMode = PictureBoxSizeMode.Normal;
+                    populateFields(path);
+                }
+                catch
+                {
+                    System.Diagnostics.Debug.WriteLine("Error cropping image");
+                }
+            }
         }
 
         private void clearFields()
@@ -123,6 +155,93 @@ namespace ImageApp
             heightTextBox.Text = Convert.ToString("");
             vResTextBox.Text = Convert.ToString("");
             hResTextBox.Text = Convert.ToString("");
+            colorsTextBox.Text = Convert.ToString("");
+            detailsListBox.Items.Clear();
         }
+
+        private void populateFields(string path)
+        {
+            if (pictureBox != null && pictureBox.Image != null)
+            {
+                cropWidthTextBox.Text = Convert.ToString(0);
+                cropHeightTextBox.Text = Convert.ToString(0);
+                cropPosX.Text = Convert.ToString(0);
+                cropPosY.Text = Convert.ToString(0);
+                widthTextBox.Text = Convert.ToString(pictureBox.Image.Width);
+                heightTextBox.Text = Convert.ToString(pictureBox.Image.Height);
+                vResTextBox.Text = Convert.ToString(pictureBox.Image.VerticalResolution);
+                hResTextBox.Text = Convert.ToString(pictureBox.Image.HorizontalResolution);
+                colorsTextBox.Text = Convert.ToString(getColors(path));
+            }
+        }
+
+        private void details(string fileName)
+        {
+            FileInfo fileInfo = new FileInfo(fileName);
+            detailsListBox.Items.Clear();
+
+            detailsListBox.Items.Add("Name: " + fileInfo.Name);
+            detailsListBox.Items.Add("Size: " + fileInfo.Length + " bytes");
+            detailsListBox.Items.Add("Location: " + fileInfo.DirectoryName);
+            detailsListBox.Items.Add("Extension: " + fileInfo.Extension);
+            detailsListBox.Items.Add("Attributes: " + fileInfo.Attributes.ToString());
+            detailsListBox.Items.Add("Creation date: " + fileInfo.CreationTime.ToShortDateString());
+            detailsListBox.Items.Add("Access date: " + fileInfo.LastAccessTime.ToShortDateString());
+            detailsListBox.Items.Add("Modification date: " + fileInfo.LastWriteTime.ToShortDateString());
+        }
+
+        private int getColors(String fileName)
+        {
+            int count = 0;
+            HashSet<Color> colors = new HashSet<Color>();
+            Bitmap bmp = null;
+
+            if (File.Exists(fileName))
+            {
+                try
+                {
+                    bmp = new Bitmap(fileName);
+                    if (bmp != null)
+                    {
+                        for (int y = 0; y < bmp.Size.Height; y++)
+                        {
+                            for (int x = 0; x < bmp.Size.Width; x++)
+                            {
+                                colors.Add(bmp.GetPixel(x, y));
+                            }
+                        }
+                        count = colors.Count;
+                    }
+                }
+                catch
+                {
+                    System.Diagnostics.Debug.WriteLine("Error getting image colors");
+                }
+                finally
+                {
+                    colors.Clear();
+                    bmp.Dispose();
+                }
+            }
+            return count;
+        }
+
+        private void cropImage(Image image, int width, int height, int posX, int posY)
+        {
+            Bitmap bmp = image as Bitmap;
+
+            Point point = new Point(posX, posY);
+            Size size = new Size(width, height);
+            Rectangle selection = new Rectangle(point, size);
+
+            if (bmp != null && width > 0 && height > 0)
+            {
+                Bitmap cropBmp = bmp.Clone(selection, bmp.PixelFormat);
+
+                image.Dispose();
+                pictureBox.Image = cropBmp;
+            }
+        }
+
     }
 }
